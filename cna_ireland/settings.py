62d49cb607745266d03a038538ee1405e527f599
Shane <36411961+ShaneCDev@users.noreply.github.com>
2023-08-14 13:29:15 +0100

    updated styling on buttons to make them more visible

--- cna_ireland/settings.py
+++ cna_ireland/settings.py
@@ -98,7 +98,7 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
-    
+
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
