180e5fbc9a2eaab5c980e05d9fa6e85df5a929f4
Shane <shaneconway1998@gmail.com>
2023-09-19 21:21:31 +0100

    plugged in the "contact" pages, finished the contact us page, created the form and updated the views accordingly

--- contact/forms.py
+++ contact/forms.py
@@ -2,6 +2,16 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    from_email = forms.EmailField(required=True)
+
+    class Meta:
+        fields = '__all__'
+
+    email = forms.EmailField(required=True)
     subject = forms.CharField(required=True)
     message = forms.CharField(widget=forms.Textarea, required=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-dark rounded-0'
--- contact/views.py
+++ contact/views.py
@@ -7,19 +7,21 @@ from django.http import HttpResponse
 
 def contact(request):
     """Contact us page"""
-    if request.method == 'GET':
-        form = ContactForm()
-    else:
-        form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
         if form.is_valid():
-            from_email = form.cleaned_data['from_email']
+            from_email = form.cleaned_data['email']
             subject = form.cleaned_data['subject']
             message = form.cleaned_data['message']
             try:
                 send_mail(subject, message, from_email, [settings.DEFAULT_FROM_EMAIL])
+                console_email = send_mail(subject, message, from_email, [settings.DEFAULT_FROM_EMAIL])
+                print(console_email)
             except BadHeaderError:
                 return HttpResponse("Invalid header.")
-            return redirect('success')
+            return redirect('contact_success')
+    else:
+        form = ContactForm()
 
     context = {
         'form': form,
