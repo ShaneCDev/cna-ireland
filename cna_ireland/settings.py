7ab6877f3da2a4c0b0139dca931828b06128f85e
Shane <36411961+ShaneCDev@users.noreply.github.com>
2023-09-26 14:03:46 +0100

    updated allowed_hosts in settings.py

--- cna_ireland/settings.py
+++ cna_ireland/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://cnaireland-6f6c57542d6c.herokuapp.com/']
 
 
 # Application definition
