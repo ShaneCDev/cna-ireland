baf2b99f0197d217afac9fb0a549fa41845f382e
Shane <shaneconway1998@gmail.com>
2023-11-17 12:02:05 +0000

    setup urls, views, footer links for the privacy policy and terms and conditions pages

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('terms-and-conditions', views.terms_and_conditions, name='terms_and_conditions'),
+    path('privacy-policy', views.privacy, name='privacy')
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,11 @@ def index(request):
     """Render the home page"""
 
     return render(request, 'home/index.html')
+
+
+def terms_and_conditions(request):
+    return render(request, 'home/terms.html')
+
+
+def privacy(request):
+    return render(request, 'home/privacy.html')
